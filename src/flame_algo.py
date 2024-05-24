91a3f4d9110f4ff93deed322c9741e00deeadd59
Yanis Moyon <yanis.moyon@epitech.eu>
2024-05-24 11:52:03 +0200

    FIX: FlameObject creates normal flame with color accuracy

--- src/flame_algo.py
+++ src/flame_algo.py
@@ -2,21 +2,18 @@ import pygame
 import os
 import random
 
-pygame.init()
-
 SCREEN_WIDTH = pygame.display.Info().current_w
 SCREEN_HEIGHT = pygame.display.Info().current_h
-FPS = 60
 
-class FlameParticule:
+class FlameParticle:
     def __init__(self, x = SCREEN_WIDTH // 2, y = SCREEN_HEIGHT // 2, r = 5, alpha_layer = 2, alpha_glow_diff = 2) -> None:
         self.x = x
         self.y = y
         self.r = r
         self.prev_r = r
-        self.alpha_layer = alpha_layer
+        self.alpha_layers = alpha_layer
         self.alpha_glow_diff = alpha_glow_diff
-        max_surf = 2 * self.r * pow(self.alpha_layer, 2) * self.alpha_glow_diff
+        max_surf = 2 * self.r * pow(self.alpha_layers, 2) * self.alpha_glow_diff
         self.surf = pygame.Surface((max_surf, max_surf), pygame.SRCALPHA)
         self.burn_rate = 0.1 * random.randint(1, 4)
 
@@ -29,48 +26,40 @@ class FlameParticule:
             self.r = 1
 
     def draw(self, screen: pygame.Surface) -> None:
-        max_surf = 2 * self.r * pow(self.alpha_layer, 2) * self.alpha_glow_diff
-        self.surf = pygame.Surface((max_surf, max_surf), pygame.SRCALPHA)
-        for i in range(self.alpha_layer, -1, -1):
-            alpha = 255 - (255 // self.alpha_layer - 5) * i
-            if (alpha < 0):
+        max_surf_size = 2 * self.r * self.alpha_layers * self.alpha_layers * self.alpha_glow_diff
+        self.surf = pygame.Surface((max_surf_size, max_surf_size), pygame.SRCALPHA)
+        for i in range(self.alpha_layers, -1, -1):
+            alpha = 255 - i * (255 // self.alpha_layers - 5)
+            if alpha <= 0:
                 alpha = 0
-            self.radius = self.r * pow(i, 2) * self.alpha_glow_diff
-            pygame.draw.circle(self.surf, (0, 0, 255, alpha), (self.surf.get_width() // 2, self.surf.get_height() // 2), self.radius)
-        screen.blit(self.surf, self.surf.get_rect(center = (self.x, self.y)))
+            radius = self.r * i * i * self.alpha_glow_diff
+            if self.r == 4 or self.r == 3:
+                r, g, b = (255, 0, 0)
+            elif self.r == 2:
+                r, g, b = (255, 150, 0)
+            else:
+                r, g, b = (50, 50, 50)
+            # r, g, b = (0, 0, 255)  # uncomment this to make the flame blue
+            color = (r, g, b, alpha)
+            pygame.draw.circle(self.surf, color, (self.surf.get_width() // 2, self.surf.get_height() // 2), radius)
+        screen.blit(self.surf, self.surf.get_rect(center=(self.x, self.y)))
 
-class FlameObject():
-    def __init__(self, x = SCREEN_WIDTH // 2, y = SCREEN_HEIGHT // 2):
+
+class FlameObject:
+    def __init__(self, x=SCREEN_WIDTH // 2, y=SCREEN_HEIGHT // 2):
         self.x = x
         self.y = y
-        self.intensity = 0
-        self.particules = []
-        for _ in range(self.intensity * 25):
-            self.particules.append(FlameParticule(self.x + random.randint(-5,5), self.y + random.randint(1,5)))
+        self.flame_intensity = 2
+        self.flame_particles = []
+        for i in range(self.flame_intensity * 25):
+            self.flame_particles.append(FlameParticle(self.x + random.randint(-5, 5), self.y, random.randint(1, 5)))
 
     def draw(self, screen: pygame.Surface):
-        for part in self.particules:
-            if part.prev_r <= 0:
-                self.particules.remove(part)
-                self.particules.append(FlameParticule(self.x + random.randint(-5,5), self.y + random.randint(1,5)))
-                del part
+        for i in self.flame_particles:
+            if i.prev_r <= 0:
+                self.flame_particles.remove(i)
+                self.flame_particles.append(FlameParticle(self.x + random.randint(-5, 5), self.y, random.randint(1, 5)))
+                del i
                 continue
-            part.update()
-            part.draw(screen)
-
-def main():
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
-    flame = FlameObject()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-
-        screen.fill((0, 0, 0))
-        flame.draw(screen)
-        pygame.display.flip()
-        clock.tick(FPS)
-
-main()
+            i.update()
+            i.draw(screen)
